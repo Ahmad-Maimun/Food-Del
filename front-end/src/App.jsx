384ca41d34436a94d679f651304d830fc61eebf8
Ahmad Maimun <ahmadmaimun.wdd@gmail.com>
2024-12-26 21:16:46 +0600

    Header Section Done

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,9 +1,18 @@
-import Navbar from "./components/navbar/Navbar";
+import { Route, Routes } from "react-router";
+import Navbar from "./components/Navbar/Navbar";
+import Cart from "./pages/Cart/Cart";
+import Home from "./pages/Home/Home";
+import PlaceOrder from "./pages/PlaceOreder/PlaceOrder";
 
 const App = () => {
   return (
     <div className="app">
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/order" element={<PlaceOrder />} />
+      </Routes>
     </div>
   );
 };
---
+++ front-end/src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header-contents">
+        <h2>Order your favorite food here</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
+          itaque earum eos consequatur! Aut voluptatem officiis laborum velit
+          cupiditate saepe illum aspernatur eum vel, itaque nihil harum
+        </p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
