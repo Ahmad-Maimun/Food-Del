ed4cbe99dee8466d551ed5ee7391053965e13bdd
Ahmad-Maimun <ahmadmaimun.wdd@gmail.com>
2025-01-22 13:18:20 +0600

    Added Footer Folder & Flie

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,19 +1,23 @@
-import { Route, Routes } from "react-router";
-import Navbar from "./components/Navbar/Navbar";
-import Cart from "./pages/Cart/Cart";
-import Home from "./pages/Home/Home";
-import PlaceOrder from "./pages/PlaceOreder/PlaceOrder";
+import { Route, Routes } from 'react-router';
+import Navbar from './components/Navbar/Navbar';
+import Cart from './pages/Cart/Cart';
+import Home from './pages/Home/Home';
+import PlaceOrder from './pages/PlaceOreder/PlaceOrder';
+import Footer from './components/Footer/Footer';
 
 const App = () => {
   return (
-    <div className="app">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/order" element={<PlaceOrder />} />
-      </Routes>
-    </div>
+    <>
+      <div className="app">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/order" element={<PlaceOrder />} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
   );
 };
 
