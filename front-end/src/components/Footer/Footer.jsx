fb511973883c26add08625101000c0b2fe5ec4a8
Ahmad-Maimun <ahmadmaimun.wdd@gmail.com>
2025-01-24 23:18:46 +0600

    Updated

--- front-end/src/components/Footer/Footer.jsx
+++ front-end/src/components/Footer/Footer.jsx
@@ -7,6 +7,12 @@ const Footer = () => {
       <div className="footer-content">
         <div className="footer-content-left">
           <img src={assets.logo} alt="" />
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia
+            repellat debitis voluptatum in facere enim maiores odit. Officiis,
+            inventore eveniet placeat nam, est dolorum aperiam dolores porro
+            ducimus reprehenderit molestiae?
+          </p>
         </div>
         <div className="footer-content-center"></div>
         <div className="footer-content-right"></div>
