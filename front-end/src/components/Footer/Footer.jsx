665d72a82c4671226abc58404fe54569b5d2faf8
Ahmad-Maimun <ahmadmaimun.wdd@gmail.com>
2025-01-25 23:35:55 +0600

    Add All Footer Content Without CSS

--- front-end/src/components/Footer/Footer.jsx
+++ front-end/src/components/Footer/Footer.jsx
@@ -19,9 +19,27 @@ const Footer = () => {
             <img src={assets.linkedin_icon} alt="" />
           </div>
         </div>
-        <div className="footer-content-center"></div>
-        <div className="footer-content-right"></div>
+        <div className="footer-content-center">
+          <h2>COMPANY</h2>
+          <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Delivery</li>
+            <li>Privacy Policy</li>
+          </ul>
+        </div>
+        <div className="footer-content-right">
+          <h2>GET IN TOUCH</h2>
+          <ul>
+            <li>+880 1894046738</li>
+            <li>fooddel@tomato.com</li>
+          </ul>
+        </div>
       </div>
+      <hr />
+      <p className="footer-copyright">
+        Copyight 2025 © Tomato.com - All Right Reserved
+      </p>
     </div>
   );
 };
