7a8f4708cd06019b17a8b152a64a45aa53014a6b
Ahmad-Maimun <ahmadmaimun.wdd@gmail.com>
2025-01-26 02:07:28 +0600

    Updated

--- front-end/src/components/AppDownload/AppDownload.jsx
+++ front-end/src/components/AppDownload/AppDownload.jsx
@@ -1,7 +1,18 @@
+import { assets } from '../../assets/assets';
 import './AppDownload.css';
 
 const AppDownload = () => {
-  return <div>AppDownload</div>;
+  return (
+    <div className="app-download" id="app-download">
+      <p>
+        For Batter Experience Download <br /> Tomato App
+      </p>
+      <div className="app-download-platforms">
+        <img src={assets.play_store} alt="" />
+        <img src={assets.app_store} alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default AppDownload;
--- front-end/src/pages/Home/Home.jsx
+++ front-end/src/pages/Home/Home.jsx
@@ -1,16 +1,18 @@
-import { useState } from "react";
-import ExploreMenu from "../../components/ExploreMenu/ExploreMenu";
-import FoodDisplay from "../../components/FoodDisplay/FoodDisplay";
-import Header from "../../components/Header/Header";
-import "./Home.css";
+import { useState } from 'react';
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu';
+import FoodDisplay from '../../components/FoodDisplay/FoodDisplay';
+import Header from '../../components/Header/Header';
+import './Home.css';
+import AppDownload from '../../components/AppDownload/AppDownload';
 
 function Home() {
-  const [category, setCategory] = useState("All");
+  const [category, setCategory] = useState('All');
   return (
     <div>
       <Header />
       <ExploreMenu category={category} setCategory={setCategory} />
       <FoodDisplay category={category} />
+      <AppDownload />
     </div>
   );
 }
