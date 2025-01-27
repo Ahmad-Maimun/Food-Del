8488eed092d413a1693ab93fa122bcaf302bac2d
Ahmad-Maimun <ahmadmaimun.wdd@gmail.com>
2025-01-28 01:42:51 +0600

    Added Scroll Behvior

--- front-end/src/components/navbar/Navbar.jsx
+++ front-end/src/components/navbar/Navbar.jsx
@@ -1,37 +1,42 @@
-import { useState } from "react";
-import { assets } from "../../assets/assets";
-import "./Navbar.css";
+import { useState } from 'react';
+import { assets } from '../../assets/assets';
+import './Navbar.css';
+import { Link } from 'react-router';
 const Navbar = () => {
-  const [menu, setMenu] = useState("home");
+  const [menu, setMenu] = useState('home');
 
   return (
     <div className="navbar">
       <img src={assets.logo} alt="" className="logo" />
       <ul className="navbar-menu">
-        <li
-          onClick={() => setMenu("home")}
-          className={menu === "home" ? "active" : ""}
+        <Link
+          to="/"
+          onClick={() => setMenu('home')}
+          className={menu === 'home' ? 'active' : ''}
         >
           Home
-        </li>
-        <li
-          onClick={() => setMenu("menu")}
-          className={menu === "menu" ? "active" : ""}
+        </Link>
+        <a
+          href="#explore-menu"
+          onClick={() => setMenu('menu')}
+          className={menu === 'menu' ? 'active' : ''}
         >
           Menu
-        </li>
-        <li
-          onClick={() => setMenu("mobile-app")}
-          className={menu === "mobile-app" ? "active" : ""}
+        </a>
+        <a
+          href="#app-download"
+          onClick={() => setMenu('mobile-app')}
+          className={menu === 'mobile-app' ? 'active' : ''}
         >
           Mobile-App
-        </li>
-        <li
-          onClick={() => setMenu("contact-us")}
-          className={menu === "contact-us" ? "active" : ""}
+        </a>
+        <a
+          href="#footer"
+          onClick={() => setMenu('contact-us')}
+          className={menu === 'contact-us' ? 'active' : ''}
         >
           Contact Us
-        </li>
+        </a>
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
