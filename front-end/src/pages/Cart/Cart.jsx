c7824f6688f3437bd52f504fafccd60e3671dbc3
Ahmad-Maimun <ahmadmaimun.wdd@gmail.com>
2025-01-29 23:02:29 +0600

    Added Context in Cart.jsx File

--- front-end/src/pages/Cart/Cart.jsx
+++ front-end/src/pages/Cart/Cart.jsx
@@ -1,6 +1,9 @@
-import "./Cart.css";
+import { useContext } from 'react';
+import './Cart.css';
+import { StoreContext } from '../../context/StoreContext';
 
 function Cart() {
+  const { cartItems, food_list, removeFromCart } = useContext(StoreContext);
   return <div></div>;
 }
 
