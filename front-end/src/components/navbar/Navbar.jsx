98bcfdd2693bdb51588fd3c2d40203939f033860
Ahmad-Maimun <ahmadmaimun.wdd@gmail.com>
2025-02-05 22:24:37 +0600

    Navbar Updated

--- front-end/src/components/navbar/Navbar.jsx
+++ front-end/src/components/navbar/Navbar.jsx
@@ -1,9 +1,11 @@
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import { assets } from '../../assets/assets';
 import './Navbar.css';
 import { Link } from 'react-router';
+import { StoreContext } from '../../context/StoreContext';
 const Navbar = ({ setShowLogin }) => {
   const [menu, setMenu] = useState('home');
+  const { getTotalCartAmount } = useContext(StoreContext);
 
   return (
     <div className="navbar">
@@ -46,7 +48,7 @@ const Navbar = ({ setShowLogin }) => {
           <Link to="/cart">
             <img src={assets.basket_icon} alt="" />
           </Link>
-          <div className="dot"></div>
+          <div className={getTotalCartAmount() === 0 ? '' : 'dot'}></div>
         </div>
         <button onClick={() => setShowLogin(true)}>Sign In</button>
       </div>
